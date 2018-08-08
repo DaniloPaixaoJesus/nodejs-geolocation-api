4c40fbac558d21ea5b6d6538f6e98f671003cb04
Danilo Paixão <danilopaixao@Admins-Mac-mini.local>
2018-08-08 19:48:52 -0300

    correcao da view da pagina principal

--- server/controllers/view.js
+++ server/controllers/view.js
@@ -1,7 +1,7 @@
 module.exports = (app)=>{
 
-  app.get('/view', (req, res)=>{
-    let resultado = app.util.Utils.somar(1,2)
+  app.get('/', (req, res)=>{
+    let resultado = 233//app.util.Utils.somar(1,2)
     res.render('index', {attr:resultado})
   });
 
