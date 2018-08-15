85852e64bcdb7037d38f0f8d0271e408adfdb412
Danilo Paixão <danilopaixao@Admins-Mac-mini.local>
2018-08-15 19:54:10 -0300

    teste

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 
-import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute, ParamMap, Router } from '@angular/router'
-import { HttpErrorResponse } from '@angular/common/http'
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -10,69 +10,69 @@ import { HttpErrorResponse } from '@angular/common/http'
 })
 export class LoginComponent {
 
-  user: String
-  password: String
-  loginError: String
+  user: String;
+  password: String;
+  loginError: String;
 
-  imgUrl = 'https://1.bp.blogspot.com/-3-AeZEPxb74/VD_YJB2D0ZI/AAAAAAAAABU/JWQpPf9BPw8/s1600/IMG_41862524318112.jpeg'
+  imgUrl = 'https://1.bp.blogspot.com/-3-AeZEPxb74/VD_YJB2D0ZI/AAAAAAAAABU/JWQpPf9BPw8/s1600/IMG_41862524318112.jpeg';
 
   constructor(
     private router: Router,
   ) { }
 
   login() {
-    console.log('efetuando login')
-    this.router.navigate(['/partner'])
+    console.log('efetuando login');
+    this.router.navigate(['/partner']);
   }
 
   reduceParaAgrupar() {
-    let guias = [
-            { peg_id: '1', guia_id: 'guia1', instance: '2342342' }, 
-            { peg_id: '2', guia_id: 'guia22', instance: '254524423' }, 
-            { peg_id: '1', guia_id: 'guia11', instance: '6967853' }, 
-            { peg_id: '2', guia_id: 'guia2', instance: '4678643' }, 
+    const guias = [
+            { peg_id: '1', guia_id: 'guia1', instance: '2342342' },
+            { peg_id: '2', guia_id: 'guia22', instance: '254524423' },
+            { peg_id: '1', guia_id: 'guia11', instance: '6967853' },
+            { peg_id: '2', guia_id: 'guia2', instance: '4678643' },
             { peg_id: '1', guia_id: 'guia111', instance: '9824612' }
-          ]
+          ];
 
-    let novoArray = guias
+    const novoArray = guias
                   .reduce((acumulador, atual) => {
                       acumulador[atual.peg_id] = acumulador[atual.peg_id] || [];
                       acumulador[atual.peg_id].push(atual);
                       return acumulador;
-                    }, 
+                    },
                     Object.create(null)
                   );
 
     console.log(novoArray);
   }
 
-  reduceParaProcurar(){
-    let tasks = [
-      { name: 'nome',id: '1', processInstance: {id:'6574534'}, taskDefinitionKey: '2342342' }, 
-      { name: 'nome',id: '1', processInstance: {id:'6574534'}, taskDefinitionKey: '2342342' }, 
-      { name: 'nome',id: '1', processInstance: {id:'6574534'}, taskDefinitionKey: '2342342' }, 
-      { name: 'nome',id: '1', processInstance: {id:'6574534'}, taskDefinitionKey: '2342342' }
-    ]
-    
-    let novoArray = tasks
+  reduceParaProcurar() {
+    const tasks = [
+      { name: 'nome', id: '1', processInstance: {id: '6574534'}, taskDefinitionKey: '2342342' },
+      { name: 'nome', id: '1', processInstance: {id: '6574534'}, taskDefinitionKey: '2342342' },
+      { name: 'nome', id: '1', processInstance: {id: '6574534'}, taskDefinitionKey: '2342342' },
+      { name: 'nome', id: '1', processInstance: {id: '6574534'}, taskDefinitionKey: '2342342' }
+    ];
+
+    const novoArray = tasks
           .filter(d => d.processInstance.id)
           .reduce( (reduce, task) => {
-                    const current = reduce.find(rd => rd.taskDefinitionKey === task.taskDefinitionKey)
-                  
+                    const current = reduce.find(rd => rd.taskDefinitionKey === task.taskDefinitionKey);
+
                     if (current) {
-                      current.processInstances.push({ taskId: task.id, ...task.processInstance })
+                      current.processInstances.push({ taskId: task.id, ...task.processInstance });
                     } else {
                       reduce.push({
                         name: task.name,
                         taskDefinitionKey: task.taskDefinitionKey,
                         processInstances: [{ taskId: task.id, ...task.processInstance }]
-                      })
+                      });
                     }
-                  
-                    return reduce
+
+                    return reduce;
               }
               , []
-            )
+            );
     console.log(novoArray);
   }
 
