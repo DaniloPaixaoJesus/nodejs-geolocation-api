726fe4f019d3010e2b8a3389a0156499f2e3f852
DaniloPaixaoJesus <contato.danilo.paixao@gmail.com>
2018-09-19 11:47:20 -0300

    api vehicles impl

--- server/api/view.js
+++ server/api/view.js
@@ -5,4 +5,13 @@ module.exports = (app)=>{
     res.render('index', {attr:resultado})
   });
 
+  app.get('/api', (req, res)=>{
+    let reponse = { 
+      name:'API Commute Project',
+      version:'1.0.0.1',
+      status:'ONLINE'
+    }
+    res.status(200).send(reponse);
+  });
+
 }
