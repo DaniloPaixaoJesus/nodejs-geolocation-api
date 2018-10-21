d63c3882c01f5bcbde77af7545d4725b1a3afee1
DaniloPaixaoJesus <contato.danilo.paixao@gmail.com>
2018-10-21 01:11:54 -0300

    api upload image

--- server/api-v1/upload.js
+++ server/api-v1/upload.js
@@ -1,19 +1,43 @@
 var fs = require('fs')
 
-module.exports = (app)=>{
+module.exports = (app) => {
 
-  let version = 2;  
+  let version = 2;
 
-  app.post(`/api/v${version}/images`, (req, res)=>{
+  var multer = require('multer');
+
+  var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'public/images/uploads')
+    },
+    filename: (req, file, cb) => {
+      // cb(null, file.fieldname + '-' + Date.now())
+      cb(null, `image-${Date.now()}.png`)
+    }
+  });
+  var upload = multer({ storage: storage });
+
+  app.post(`/api/v${version}/images`, upload.single('file-upload'), (req, res) => {
+    let reponse = { 
+      filename: req.file.filename,
+      path: `public/images/uploads/${req.file.filename}`,
+      uri: `/api/v${version}/images/${req.file.filename}`
+    }
+    res.status(200).send(reponse);
+    //res.redirect('/');
+  });
+
+
+  app.post(`/api/v${version}/images-old`, (req, res) => {
     console.log('recebendo imagem')
 
     var filename = req.headers.filename;
 
     req.pipe(fs.createWriteStream('files/' + filename))
-    .on('finish', ()=>{
-      console.log('arquivo escrito');
-      res.status(201).send('ok');
-    })
+      .on('finish', () => {
+        console.log('arquivo escrito');
+        res.status(201).send('ok');
+      })
 
   })
 
